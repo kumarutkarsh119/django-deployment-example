12c026456f50aad84212a8dd5901b51eaa103b27
Utkarsh Kumar <utkarshkumar@Utkarshs-MacBook-Pro.local>
2020-04-11 03:08:14 +0530

    g

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import re_path
+from basic_app import views
+
+#Template urls
+app_name = 'basic_app'
+
+urlpatterns=[
+    re_path(r'^register/$',views.register,name = 'register'),
+    re_path(r'^user_login/$',views.user_login,name = 'user_login')
+]
